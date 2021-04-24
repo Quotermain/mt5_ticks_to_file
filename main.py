272d494931529639091cdc935a3eba413fc448ee
Quotermain <>
2021-04-24 17:23:22 +0500

    Adds logic to iterate over tickers with problems during the main iteration cycle

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from utils.get_tqbr_tickers import get_tqbr_tickers
 from datetime import datetime
 import MetaTrader5 as mt5
 import pandas as pd
-from utils.get_tqbr_tickers import get_tqbr_tickers
+import pickle
+from sys import argv
 
-ALL_TICKERS = get_tqbr_tickers()
+if argv[1] == 'problem_tickers':
+    with open('problem_tickers.pickle', 'rb') as file:
+        ALL_TICKERS = pickle.load(file)
+elif argv[1] == 'all_tickers':
+    ALL_TICKERS = get_tqbr_tickers()
 
+problem_tickers = []
 def run(ticker):
     """
     Establishes connection with the terminal, parses prices,
@@ -14,8 +21,8 @@ def run(ticker):
         print("initialize() failed, error code =", mt5.last_error())
         quit()
 
-    utc_from = datetime(2021, 1, 1)
-    utc_to = datetime(2021, 4, 11)
+    utc_from = datetime(2020, 12, 1)
+    utc_to = datetime(2021, 4, 23)
     ticks = mt5.copy_ticks_range(ticker, utc_from, utc_to, mt5.COPY_TICKS_TRADE)
     mt5.shutdown()
 
@@ -29,4 +36,10 @@ def run(ticker):
 
 if __name__ == '__main__':
     for ticker in ALL_TICKERS:
-        run(ticker)
+        try:
+            run(ticker)
+        except Exception as e:
+            problem_tickers.append(ticker)
+            continue
+    with open('problem_tickers.pickle', 'wb') as file:
+        pickle.dump(problem_tickers, file)
